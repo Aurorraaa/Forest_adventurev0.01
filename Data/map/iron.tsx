c896fb45b0b9b261972b7db8b9c61e7b96f3315e
Huligan4ik2007 <user@gmail.com>
2025-01-15 00:35:50 +0500

    help!

---
+++ Data/map/iron.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="iron" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="tiles/Mining Nodes/32x32/C_Nodes10.png" width="32" height="32"/>
+</tileset>
